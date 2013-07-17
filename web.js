7c38661a869395897973769e9747d0c69628aee7
haystacker2000 <dhay075@aucklanduni.ac.nz>
2013-07-17 08:40:29 +0000

    Added a second version of index.html, index2.html, and updated web.js to use it.

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-var page = fs.readFileSync("index.html", 'utf8');
+var page = fs.readFileSync("index2.html", 'utf8');
 
 var buf = new Buffer(page, 'utf8');
 
@@ -12,7 +12,7 @@ app.get('/', function(request, response) {
   response.send(buf.toString('utf8', 0, buf.length));
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
